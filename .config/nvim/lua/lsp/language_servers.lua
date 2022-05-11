b6d0216ba36540377edf3700bd0748453f4737ff
SheetaI <im.kawaii.gg@gmail.com>
2022-05-12 02:59:33 +0800

    Update language_servers.lua

--- .config/nvim/lua/lsp/language_servers.lua
+++ .config/nvim/lua/lsp/language_servers.lua
@@ -1,21 +1,12 @@
-local lsp_installer = require('nvim-lsp-installer')
+require("nvim-lsp-installer").setup {}
+local lspconfig = require("lspconfig")
 
-lsp_installer.on_server_ready(function(server)
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol
-                                                                       .make_client_capabilities())
-  local opts = {capabilities = capabilities}
-  if server.name == "sumneko_lua" then
-    opts = vim.tbl_deep_extend("force", {
-      settings = {
-        Lua = {
-          runtime = {version = 'LuaJIT', path = vim.split(package.path, ';')},
-          diagnostics = {globals = {'vim'}},
-          workspace = {library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false},
-          telemetry = {enable = false}
-        }
-      }
-
-    }, opts)
-  end
-  server:setup(opts)
-end)
+lspconfig.sumneko_lua.setup {}
+lspconfig.tsserver.setup {}
+lspconfig.bashls.setup {}
+lspconfig.pyright.setup {}
+lspconfig.rust_analyzer.setup {}
+lspconfig.tailwindcss.setup {}
+lspconfig.clangd.setup {}
+lspconfig.cmake.setup {}
+lspconfig.html.setup {}
