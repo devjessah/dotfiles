28a893629e72a23914e212884296ea6f150b2d19
SheetaI <im.kawaii.gg@gmail.com>
2022-05-12 04:02:17 +0800

    Update language_servers.lua

--- .config/nvim/lua/lsp/language_servers.lua
+++ .config/nvim/lua/lsp/language_servers.lua
@@ -1,7 +1,15 @@
 require("nvim-lsp-installer").setup {}
 local lspconfig = require("lspconfig")
 
-lspconfig.sumneko_lua.setup {}
+lspconfig.sumneko_lua.setup({
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
+})
 lspconfig.tsserver.setup {}
 lspconfig.bashls.setup {}
 lspconfig.pyright.setup {}
